945822b01595ca504a7bef8d47d2a9e17fbe36c9
evrothman <evrothman@davidson.edu>
2021-09-12 21:18:27 -0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,11 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+document.addEventListener("mousemove", () => {
+  let mousex = event.clientX;
+  let mousey = event.clientY;
+  document.getElementById("mouse").innerHTML = "X: " + mousex + ", " + "Y: " + mousey;
+});
+
+var elements = document.querySelectorAll("div").length;
+console.log("Number of Element in Self Portrait: " + elements);
